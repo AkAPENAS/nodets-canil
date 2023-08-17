bdd122e52ff8e9a45bb9c65a670b53904fa56aee
Ak <luizleandrogato@gmail.com>
2023-08-17 00:27:52 -0300

    final

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,6 +9,6 @@ router.get('/dogs',pageController.dogs);
 router.get('/cats',pageController.cats);
 router.get('/fitshes',pageController.fitshes);
 
-router.get('/search',searchController.search);
+router.get('/serach',searchController.search);
 
 export default router;
