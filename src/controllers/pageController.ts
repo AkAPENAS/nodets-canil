394dcc0f7e3d7c136b7421f2485a9759cdf3c4e4
LUIZ ROBERTO LEANDRO BARBOSA <luizleandrogato@gmail.com>
2023-08-16 22:10:16 -0300

    subindo para terminar codigo em outro dispositivo

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,19 +1,45 @@
 import { Request, Response } from "express";
+import {createMenuObject} from '../helpers/createMenuObject'
+
 
 export const home = (req:Request, res:Response)=> {
-    res.render('../views/pages/page')
+    res.render('../views/pages/page',{
+        menu:createMenuObject('all'),
+        banner:{
+            title:'all pets',
+            background: 'allanimals.jpg'
+        }
+    })
     //res.render('pages/page')
 };
 
 export const dogs = (req:Request, res:Response)=> {
-    res.send('dogs in controller')
+    res.render('../views/pages/page',{
+        menu: createMenuObject("dog"),
+        banner:{
+            title:'dogs',
+            background: 'banner_dog.jpg'
+        }
+    })
     //res.render('pages/page')
 };
 
 export const cats = (req:Request, res:Response)=> {
-    //res.render('pages/page')
+    res.render('../views/pages/page',{
+        menu: createMenuObject('cat'),
+        banner:{
+            title:'cats',
+            background: 'banner_cat.jpg'
+        }
+    })
 };
 
 export const fitshes = (req:Request, res:Response)=> {
-    //res.render('pages/page')
+    res.render('../views/pages/page',{
+        menu: createMenuObject('fish'),
+        banner:{
+            title:'fitshes',
+            background: 'banner_fish.jpg'
+        }
+    })
 };
